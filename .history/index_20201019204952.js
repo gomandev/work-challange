3b4ec84a1349a7c610f670f6bda73b440a5a0542
Gomandev <gomandev@gmail.com>
2020-10-19 20:56:14 +0100

    resolve conflict

--- .history/index_20201019204952.js
+++
@@ -1,7 +0,0 @@
-var name = require('./package.json').name
-require('productionize')(name)
-
-var server = require('./lib/server')
-var port = process.env.PORT || 5000
-server().listen(port)
-console.log(name, 'listening on port', port)
