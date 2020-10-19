96593b3442891f371043313b9efee11b9304cae2
Gomandev <gomandev@gmail.com>
2020-10-19 20:46:33 +0100

    done add reverse

--- .history/index_20201019200930.js
+++ .history/index_20201019200930.js
@@ -4,4 +4,4 @@ require('productionize')(name)
 var server = require('./lib/server')
 var port = process.env.PORT || 5000
 server().listen(port)
-console.log(name, 'listening on', port)
+console.log(name, 'listening on port', port)
